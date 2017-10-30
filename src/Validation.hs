5cd688bcc0aa62e2262c3f176a98ee732213d645
Deepak Kapiswe <deepakkapiswe@gmail.com>
2017-10-30 12:21:18 +0530

    added little documentation

--- src/Validation.hs
+++ src/Validation.hs
@@ -3,11 +3,16 @@ module Validation
      where
 import Data.Char (isDigit)
 
+-- | validate str will validate the length and characters 
+--   of the input str and call check function for further
+--   analysis if input is valid
+validateStr::String->Bool
+validateStr inp | (length inp == 16) && (all isDigit inp)  = check inp
+                | otherwise = False
 
-validate::String->Bool
-validate inp | (length inp == 16) && (all isDigit inp)  = check inp
-             | otherwise = False
-
+-- | check the main function to do all the validations 
+--   according to Luan Algo, Input is supposed to be a
+--   string of length 16 and all characters as digits
 check::String->Bool
 check ls = rem digitSum 10 == 0
  where
@@ -15,9 +20,15 @@ check ls = rem digitSum 10 == 0
    sumAll acc (idx,x) = (sum.digits.show $ f idx x) + acc
    zipped = zip [1..] (digits ls)
 
+
+-- | digits will return a list of Int the input number is
+--   composed of
 digits::String->[Int]
 digits =fmap (read.pure)
 
+-- | An auxiliary function f to check and double the value
+--   if the index is odd i.e. each alternate elements from
+--   right
 f::Int->Int->Int
 f idx val | odd idx = 2*val
           | otherwise = val
@@ -25,7 +36,7 @@ f idx val | odd idx = 2*val
 validateCard :: IO ()
 validateCard = do
   inp <- getLine
-  let result = case validate inp of
+  let result = case validateStr inp of
                  True -> " is valid"
                  _->" is not valid"
   putStrLn $ "Credit Card "++inp++result
